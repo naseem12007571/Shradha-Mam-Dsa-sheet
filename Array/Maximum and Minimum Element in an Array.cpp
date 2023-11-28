d827dc32f78aea111995b3a90f45b1d4310e67d9
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-11-29 00:13:12 +0530

    GFG

--- Array/Maximum and Minimum Element in an Array.cpp
+++ Array/Maximum and Minimum Element in an Array.cpp
@@ -20,3 +20,27 @@ public:
         return ans + ans2;
     }
 };
+
+
+compilor code 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    
+    int arr[6]= {15,30,2,3,6,8};
+    int ans = INT_MAX;
+    int ans2= INT_MIN;
+    for(int i=0;i<5;i++){
+        if(arr[i]<ans)
+        ans=arr[i];
+    }
+    for(int i=0;i<5;i++){
+    if(arr[i]>ans2){
+        ans2=arr[i];
+    }
+    }
+    cout<<"max value is "<<ans2<<endl;
+    cout<<"min value is "<<ans;
+}
